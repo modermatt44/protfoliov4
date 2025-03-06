40f1c7ce4333d73da0a7484ec4f1618fd4018fae
marco <marco.odermatt56@gmail.com>
2025-03-06 21:44:13 +0100

    added react routing and frontend template

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <section id="home" className="App-section">
+            <h2>Home</h2>
+            <p>Welcome to my portfolio!</p>
+        </section>
+    )
+}
+
+export default Home
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <section id="contact" className="App-section">
+            <h2>Contact</h2>
+            <p>Feel free to reach out to me at marco@example.com.</p>
+        </section>
+    )
+}
+
+export default Contact
